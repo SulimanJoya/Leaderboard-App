d8b65f15be15574d9eb0e124a538376a4dcce189
Mohammad Suliman joya <sjoya66@gmail.com>
2023-04-13 20:37:34 +0430

    add url file and add API url

---
+++ src/utils/url.js
@@ -0,0 +1,7 @@
+const url =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ZXdiZTI7iQwVr3io3WC5/scores';
+
+const newGameUrl =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+
+export { url, newGameUrl };
